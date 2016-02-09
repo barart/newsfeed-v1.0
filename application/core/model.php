3a32b1a10f2ca9933ff2b775bc0eaa227c33c7f4
barart <darakon92@gmail.com>
2016-02-09 16:54:37 +0400

    Working with Model class

--- application/core/model.php
+++ application/core/model.php
@@ -11,15 +11,28 @@ class Model
 			> методы для работы с NoSQL;
 			> и др.
 	*/ 
-	protected $dbhost = 'localhost'; 
+	protected $dbhost = '22localhost'; 
 	protected $dbname = 'newsfeed'; 
-	protected $dbuser = 'newsfeed_user'; 
+	protected $dbuser = 'newsfeed'; 
 	protected $dbpass = '55831421'; 
 	
 	function __construct()
 	{
-		
+		$dsn = "mysql:host=$this->dbhost;dbname=$this->newsfeed";
+		$opt = array(
+		    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+		    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+		);
+
+		try {
+			$pdo = new PDO($dsn, $this->dbuser, $this->dbpass, $opt);
+		} catch (PDOException $e){
+			// die ('Подключение не удалось: ' . $e->getMessage());
+			Route::ErrorPage404();
+		}
+
 	}		
+	
 	// метод выборки данных
 	public function get_data()
 	{
